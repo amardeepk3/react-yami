d97d4b0ff16ce077535d2d1f06d9583cf0db0a6a
amardeep kumar <amardeepk3@gmail.com>
2024-01-19 21:00:44 +0530

    did initial setup

--- App.js
+++ App.js
@@ -1,3 +1,42 @@
-const header = React.createElement("h1", {id:"heading"}, "hello world from react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/*
+ header
+ - logo
+ - nav bar
+ body
+ - 
+ footer
+*/
+// const header = React.createElement("h1", {id:"heading"}, "hello world from react");
+const jsxHeading = (
+    <h1 id="heading">hello world</h1>)
+    ;
+
+const Header = (props) => {
+    return (
+        <div className="header">
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+const Body = () =>{
+    return (
+        <div>
+        </div>
+    );
+}
+
+const Layout = ()=>{
+    return (<div>
+<Header title="header"/>
+
+<Body/>
+    </div>);
+}
+
+const HeadComponent = () => (<h1 id="heading">hello world</h1>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(header);
+root.render(< Layout />);
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # react yami project
+# git brnch -M main
+# npm i -D parcel
+# npx parcel index.html   // for executing package using npx and npm is used for installing packages
+
+# parcel 
+- dev build
+- Local server
+-HMR = Hot Module Replacement
+- using file watchinng algorithm writtn in c++
+- caching - faster build
+- image optimization
+- minification
+- bundeling
+- compressing 
+- consisting hasing
+
+
+
+# React uses
+- reconciliation algorithm(React fiber)
+-  
+
+# shimmer ui
+
+# css
+- npm install -D tailwindcss postcss
+- npx tailwindcss init
