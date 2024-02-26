c2861cb743f94ac569d09c266c080fd1fbabf561
amardeep kumar <amardeepk3@gmail.com>
2024-02-26 17:50:48 +0530

    added landing and login page

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import 'semantic-ui-css/semantic.min.css'
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+
+import HomepageLayout from "./src/components/HomepageLayout"
+import LoginForm from "./LoginForm";
+import Footer from "./src/components/Footer"
 
 /*
  header
@@ -10,33 +17,59 @@ import ReactDOM from "react-dom/client";
  footer
 */
 // const header = React.createElement("h1", {id:"heading"}, "hello world from react");
-const jsxHeading = (
-    <h1 id="heading">hello world</h1>)
-    ;
+// const jsxHeading = (
+//     <h1 id="heading">hello world</h1>)
+//     ;
 
-const Header = (props) => {
-    return (
-        <div className="header">
-            <h1>{props.title}</h1>
-        </div>
-    )
-}
+// const Header = (props) => {
+//     return (
+//         <div className="header">
+//             <h1>{props.title}</h1>
+//         </div>
+//     )
+// }
 
-const Body = () =>{
-    return (
-        <div>
-        </div>
-    );
-}
+// const Body = () =>{
+//     return (
+//         <div>
+//         </div>
+//     );
+// }
 
-const Layout = ()=>{
-    return (<div>
-<Header title="header"/>
+// const Layout = ()=>{
+//     return (<div>
+// <Header title="header"/>
 
-<Body/>
-    </div>);
-}
+// <Body/>
+//     </div>);
+// }
 
-const HeadComponent = () => (<h1 id="heading">hello world</h1>);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(< Layout />);
+// const HeadComponent = () => (<h1 id="heading">hello world</h1>);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(< HomepageLayout />);
+// const AppLayout = () => {
+
+//     return (
+    
+//     <><HomepageLayout /><Outlet /> <Footer /> </>
+//     );
+//   };
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomepageLayout />,
+      errorElement: <Error />,
+      children: [
+
+      ],
+    },
+    {
+      path: "/login",
+      element: <LoginForm />,
+    }
+  ]);
+  
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  
+  root.render(<RouterProvider router={appRouter} />);
+  
